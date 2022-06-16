72b1ec69b25737e1389aa338a5977523cf773be0
Jim Paine <jim@jimpaine.io>
2022-06-16 19:08:38 +0100

    removed unused using

--- src/functionapp/Helpers.cs
+++ src/functionapp/Helpers.cs
@@ -1,7 +1,6 @@
 using System.Text.Json;
 using functionapp.Models;
 using System.ComponentModel.DataAnnotations;
-using System.Security.Cryptography.X509Certificates;
 using Microsoft.AspNetCore.Http;
 using System.Security.Cryptography;
 
